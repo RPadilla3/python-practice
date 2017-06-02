7b60fbaf320ccca10178744efeebab08f893d8fb
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2017-06-01 22:10:34 -0400

    Added conditionals to quit the program if the character limit of lorem ipsum was above twitters 140char limit.

--- selenium/tweet.py
+++ selenium/tweet.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
+import time
 
-twitter_password = ''
+twitter_password = 'gordo64'
 
 path = '/Users/Rodolfo/Desktop/chrome-bot/chromedriver'
 
@@ -10,15 +11,24 @@ option.add_argument('--incognito')
 driver = webdriver.Chrome(path, chrome_options=option)
 
 driver.get("http://www.lipsum.com/")
+
 word_clicker = driver.find_element_by_xpath("""//*[@id="words"]""").click()
 word_size_click = driver.find_element_by_xpath("""//*[@id="amount"]""").click()
 word_size = driver.find_element_by_xpath("""//*[@id="amount"]""").clear()
 word_size_choice = driver.find_element_by_xpath("""//*[@id="amount"]""")
 word_size_choice.send_keys('20')
+
 lorem_create = driver.find_element_by_xpath("""//*[@id="generate"]""").click()
 paragraph = driver.find_element_by_css_selector('#lipsum')
 
 new_paragraph = str(paragraph.text)
+
+if len(new_paragraph) < 140:
+    print('Tweet is Ok' + '\n')
+else:
+    print('Tweet is too long')
+    time.sleep(10)
+    driver.quit()
 
 driver.get('https://twitter.com/')
 
@@ -41,3 +51,5 @@ tweet_input = driver.find_element_by_xpath("""//*[@id="tweet-box-home-timeline"]
 tweet_input.send_keys(new_paragraph)
 send_tweet = driver.find_element_by_xpath(
     """//*[@id="timeline"]/div[2]/div/form/div[3]/div[2]/button""").click()
+
+print("You Successfully Tweeted " + new_paragraph + " to your Twitter Account!")
